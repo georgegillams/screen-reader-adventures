a439cbe7ebd042fc76ea8c110222e8d1dad845a1
George Gillams <george.gillams@skyscanner.net>
2019-05-17 18:24:46 +0100

    Update macos instructions

--- app/containers/Level1/Level1Page.js
+++ app/containers/Level1/Level1Page.js
@@ -7,20 +7,35 @@ import STYLES from '../pages.scss';
 
 const getClassName = cssModules(STYLES);
 
-const Level1Page = () => {
+const Level1Page = props => {
+  const { className, ...rest } = props;
+
+  const outerClassNameFinal = [getClassName('pages__container')];
+
+  if (className) {
+    outerClassNameFinal.push(className);
+  }
+
   return (
-    <LevelWrapper
-      levelNumber={1}
-      description={getCopy('level1Description')}
-      level={[
-        ['a', 's', 's', 's', 'a'],
-        ['b', 'b', 'b', 'b', 's'],
-        ['b', 'b', 'b', 'b', 's'],
-        ['b', 'b', 'b', 'b', 's'],
-        ['b', 'b', 'b', 'b', 'g'],
-      ]}
-      startSpace={{ x: 0, y: 0 }}
-    />
+    <div
+      className={`${getClassName('pages__container')} ${getClassName(
+        'pages__container--centered',
+      )}`}
+      {...rest}
+    >
+      <LevelWrapper
+        levelNumber={1}
+        description={getCopy('level1Description')}
+        level={[
+          ['a', 's', 's', 's', 'a'],
+          ['b', 'b', 'b', 'b', 's'],
+          ['b', 'b', 'b', 'b', 's'],
+          ['b', 'b', 'b', 'b', 's'],
+          ['b', 'b', 'b', 'b', 'g'],
+        ]}
+        startSpace={{ x: 0, y: 0 }}
+      />
+    </div>
   );
 };
 
