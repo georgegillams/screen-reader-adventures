c2261645969bbe0de9bc33d95b1a14d27c3ac92f
George Gillams <george.gillams@skyscanner.net>
2019-05-17 08:26:54 +0100

    Implement game play logic

--- app/components/Levels/LevelWrapper.js
+++ app/components/Levels/LevelWrapper.js
@@ -3,6 +3,7 @@ import { findDOMNode } from 'react-dom';
 import { Helmet } from 'react-helmet';
 import { Section } from 'components/Typography';
 import GGButton from 'components/GGButton';
+import { OpenSpace, Space, GoalSpace, BlankSpace } from 'components/Levels';
 import { cssModules } from 'bpk-react-utils';
 
 import Character from './Character';
@@ -14,15 +15,54 @@ export default class LevelWrapper extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { levelComplete: false, characterPos: { left: 0, top: 0 } };
+    this.state = {
+      levelComplete: false,
+      characterGamePos: { x: 0, y: 0 },
+      characterPos: { left: 0, top: 0 },
+    };
     this.character = React.createRef();
+    this.spaceRefs = {};
   }
 
-  summonCharacter = sender => {
-    // TODO Move to next square one at a time.
+  onLevelComplete = () => {
+    this.setState({ levelComplete: true });
+  };
+
+  componentDidMount = () => {
+    const { startSpace } = this.props;
+    if (!startSpace) {
+      return;
+    }
+
+    const { x, y } = startSpace;
+    this.setState({ characterGamePos: startSpace });
+    if (
+      this.spaceRefs &&
+      this.spaceRefs[`row_${x}`] &&
+      this.spaceRefs[`row_${x}`][`col_${y}`]
+    ) {
+      const ref = this.spaceRefs[`row_${x}`][`col_${y}`];
+      this.moveCharacter(ref);
+    }
+  };
+
+  summonCharacter = (x, y, sender) => {
+    const { characterGamePos } = this.state;
+    // Only permit moving in straight lines
+    if (x !== characterGamePos.x && y !== characterGamePos.y) {
+      return;
+    }
+    // TODO Move to next square one at a time, instead of jumping.
     // TODO On each square, call `onVisit`
-    // TODO Add game logic to determine if the character can move.
-    // TODO Also to determine if character has one, or died
+    // TODO `OnVisit` should determine whether the character has won, or died
+    this.moveCharacter(sender);
+    if (sender.current.props.onVisit) {
+      sender.current.props.onVisit();
+    }
+    this.setState({ characterGamePos: { x, y } });
+  };
+
+  moveCharacter = sender => {
     const newPos = {};
     const square = findDOMNode(sender.current);
     const squarePos = square.getBoundingClientRect();
@@ -31,16 +71,18 @@ export default class LevelWrapper extends Component {
     this.setState({ characterPos: newPos });
   };
 
-  render() {
-    const { levelNumber, description, level, ...rest } = this.props;
+  createSpaceRef = (i, j) => {
+    const newRef = React.createRef();
+    const rowRefs = this.spaceRefs[`row_${i}`];
+    if (!rowRefs) {
+      this.spaceRefs[`row_${i}`] = {};
+    }
+    this.spaceRefs[`row_${i}`][`col_${j}`] = newRef;
+    return newRef;
+  };
 
-    const levelClone = React.cloneElement(level, {
-      onLevelComplete: () => {
-        this.setState({ levelComplete: true });
-      },
-      className: getClassName('level-wrapper__level'),
-      summonCharacter: this.summonCharacter,
-    });
+  render() {
+    const { startSpace, levelNumber, description, level, ...rest } = this.props;
 
     return (
       <Section
@@ -52,7 +94,47 @@ export default class LevelWrapper extends Component {
         <span className={getClassName('level-wrapper__description')}>
           {description}
         </span>
-        {levelClone}
+        <div className={getClassName('level-wrapper__level')}>
+          {level.map((row, i) => (
+            <div className={getClassName('level-wrapper__row')}>
+              {row.map((spaceTypeId, j) => {
+                const spaceRef = this.createSpaceRef(i, j);
+                if (spaceTypeId === 'a') {
+                  return (
+                    <Space
+                      spaceNumber={i}
+                      ref={spaceRef}
+                      onClick={() => this.summonCharacter(i, j, spaceRef)}
+                    />
+                  );
+                }
+                if (spaceTypeId === 's') {
+                  return (
+                    <OpenSpace
+                      spaceNumber={i}
+                      ref={spaceRef}
+                      onClick={() => this.summonCharacter(i, j, spaceRef)}
+                    />
+                  );
+                }
+                if (spaceTypeId === 'b') {
+                  return <BlankSpace spaceNumber={i} />;
+                }
+                if (spaceTypeId === 'g') {
+                  return (
+                    <GoalSpace
+                      spaceNumber={i}
+                      onVisit={() => this.onLevelComplete()}
+                      ref={spaceRef}
+                      onClick={() => this.summonCharacter(i, j, spaceRef)}
+                    />
+                  );
+                }
+              })}
+              <br />
+            </div>
+          ))}
+        </div>
         <Character ref={this.character} style={this.state.characterPos} />
         <GGButton
           large
