719f813edf6fb6edd2f72c26793d0ba999e59a7e
George Gillams <george.gillams@skyscanner.net>
2020-05-05 16:52:05 +0100

    [NO-JIRA] Fix tests

--- config/jest.config.js
+++ config/jest.config.js
@@ -25,5 +25,5 @@ module.exports = {
     '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/config/jest-mocks/image.js',
   },
-  setupTestFrameworkScriptFile: '<rootDir>/config/test-setup.js',
+  setupFilesAfterEnv: ['<rootDir>/config/test-setup.js'],
 };
--- helpers/constants.js
+++ helpers/constants.js
@@ -8,7 +8,7 @@ const UNAME_REGEX = /^[A-Za-z0-9\.\ ]*$/gi;
 const REDIRECT_REGEX = /^[A-Za-z0-9\.\ \-\_#\/]*$/gi;
 const NAME_REGEX = /^[A-Za-z\ ]*$/gi;
 const MONZOME_LINK_REGEX = /^(https?:\/\/)?monzo\.me\/[A-Za-z_-]+(\/.*)?$/gi;
-const NON_EMOJI_REGEX = /[A-Za-z\ 0-9,]*/gi;
+const NON_EMOJI_REGEX = /[A-Za-zä\ 0-9,]*/gi;
 const EMAIL_REGEX = /^([a-zA-Z0-9_\-\.\+]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/gi;
 const PASSWORD_REGEX = /.{6,}/gi;
 const MD_COMPLETE_REGEX = /.*/gi; // TODO UPDATE
@@ -40,28 +40,29 @@ const COOKIE_NAMES = [
   'userComments',
 ];
 const EMAIL_TAKEN = {
-  error: 'Email already taken.',
+  error: 'invalid-request',
+  errorMessage: 'Email already taken.',
 };
 const INVALID_SESSION = {
-  error: 'authentication',
-  reason:
+  error: 'auth',
+  errorMessage:
     'Invalid session. Try clearing cookies for this site and then re-authenticate',
 };
 const INVALID_CREDENTIALS = {
-  error: 'authentication',
-  reason: 'Error logging in. The credentials supplied are invalid.',
+  error: 'wrong-input',
+  errorMessage: 'Error logging in. The credentials supplied are invalid.',
 };
 const UNAUTHORISED_READ = {
-  error: 'authentication',
-  reason: 'You are not authorised to read this resource',
+  error: 'auth',
+  errorMessage: 'You are not authorised to read this resource',
 };
 const UNAUTHORISED_WRITE = {
-  error: 'authentication',
-  reason: 'You are not authorised to write to this resource',
+  error: 'auth',
+  errorMessage: 'You are not authorised to write to this resource',
 };
 const RESOURCE_NOT_FOUND = {
-  error: '404',
-  reason:
+  error: 'not-found',
+  errorMessage:
     "We looked everywhere but we couldn't find that resource. Maybe you need to sign in.",
 };
 const CHECK_FOR_NEW_CONTENT_INTERVAL = 1000;
@@ -74,25 +75,38 @@ const EMAIL_HTML_BUTTON_STYLE =
   'background-color: #e02626;padding: .9rem 1.2rem;color: white;border-radius: 2rem;text-decoration: none;';
 const EMAIL_SENDER_EMAIL = GG_EMAIL;
 const PROJECT_UNDER_TEST = process.env.PROJECT_UNDER_TEST;
+const PORT = process.env.PORT || 3000;
 const AWS = process.env.AWS === 'true';
 const NODE_ENV = process.env.NODE_ENV;
 const SITE_URL =
   NODE_ENV === 'development' || PROJECT_UNDER_TEST
-    ? 'http://localhost:3000'
+    ? `http://localhost:${PORT}`
     : 'https://www.screen-reader-adventures.com';
 const API_ENDPOINT = `${SITE_URL}/api`;
+const GENERIC_ERROR_MESSAGE = {
+  type: 'error',
+  message: 'Something went wrong. Please try again later.',
+};
 const COMMUNICATION_ERROR_MESSAGE = {
   type: 'error',
   message: 'Our servers are poorly. Please try again later.',
 };
-// const STRIPE_PUBLIC_API_KEY = 'pk_live_0Rqmq4ff2LdGWhZQXs1fsQCi';
-const STRIPE_PUBLIC_API_KEY = 'pk_test_HA7gWIqWYkfh2Dc6OcWHO43T';
+const STRIPE_PUBLIC_API_KEY = process.env.STRIPE_PUBLIC_API_KEY
+  ? process.env.STRIPE_PUBLIC_API_KEY
+  : 'pk_test_cgQazYLEKCzNFGFuRfq0TL8N00Cj3LIfai';
+const STRIPE_SECRET_API_KEY = process.env.STRIPE_SECRET_API_KEY;
 const USERNAMES_ENABLED = PROJECT_NAME !== 'EPICC';
-const REDIS_INFORMATION_STORES = ['notifications', 'users'];
+const REDIS_INFORMATION_STORES = [
+  'analytics',
+  'notifications',
+  'payments',
+  'stripepayments',
+  'users',
+];
 const REDIS_STORES = [
   ...REDIS_INFORMATION_STORES,
   'emailVerificationCodes',
-  'magicLinks',
+  'magiclinks',
   'sessions',
 ];
 
@@ -120,6 +134,7 @@ export {
   EMAIL_TAKEN,
   EMAIL_VERIFICATION_ENABLED,
   EXPIRY_REGEX,
+  GENERIC_ERROR_MESSAGE,
   GG_EMAIL,
   ID_REGEX,
   INT_REGEX,
@@ -134,15 +149,17 @@ export {
   NON_EMOJI_REGEX,
   PASSWORD_REGEX,
   PROJECT_NAME,
-  PROJECT_UNDER_TEST,
-  REDIS_INFORMATION_STORES,
-  REDIS_STORES,
   REDIRECT_REGEX,
   RESOURCE_NOT_FOUND,
   SITE_URL,
   SORT_CODE_REGEX,
   STRING_REGEX,
   STRIPE_PUBLIC_API_KEY,
+  STRIPE_SECRET_API_KEY,
+  PORT,
+  PROJECT_UNDER_TEST,
+  REDIS_INFORMATION_STORES,
+  REDIS_STORES,
   TICKET_COST_EB_ONE_DAY,
   TICKET_COST_EB_TWO_DAY,
   TICKET_COST_R_ONE_DAY,
@@ -178,6 +195,7 @@ export default {
   EMAIL_TAKEN,
   EMAIL_VERIFICATION_ENABLED,
   EXPIRY_REGEX,
+  GENERIC_ERROR_MESSAGE,
   GG_EMAIL,
   ID_REGEX,
   INT_REGEX,
@@ -192,15 +210,17 @@ export default {
   NON_EMOJI_REGEX,
   PASSWORD_REGEX,
   PROJECT_NAME,
-  PROJECT_UNDER_TEST,
-  REDIS_INFORMATION_STORES,
-  REDIS_STORES,
   REDIRECT_REGEX,
   RESOURCE_NOT_FOUND,
   SITE_URL,
   SORT_CODE_REGEX,
   STRING_REGEX,
   STRIPE_PUBLIC_API_KEY,
+  STRIPE_SECRET_API_KEY,
+  PORT,
+  PROJECT_UNDER_TEST,
+  REDIS_INFORMATION_STORES,
+  REDIS_STORES,
   TICKET_COST_EB_ONE_DAY,
   TICKET_COST_EB_TWO_DAY,
   TICKET_COST_R_ONE_DAY,
